3ed5a758532ec7376e03e18057a3ee5c3fb37e4e
Andrew Heiss <andrewheiss@gmail.com>
2014-04-27 00:28:45 -0400

    Add confidence intervals

--- uds-simulation.R
+++ uds-simulation.R
@@ -42,16 +42,28 @@ simulated.coefs.long <- melt(simulated.coefs) %.%
   mutate(coef.name=factor(sub("uds.sim\\d+", "uds.sim", Var1))) %.%
   select(coef.name, parameter=Var2, estimate=value, simulation=L1)
 
+coefs.err <- simulated.coefs.long %.%
+  filter(parameter=="Std. Error" & (coef.name=="uds.sim" | coef.name=="icrg_qog"))
+
+std.errs <- coefs.err$estimate
+
 # Select data to plot
 coefs.plot <- simulated.coefs.long %.%
   filter(parameter=="Estimate" & (coef.name=="uds.sim" | coef.name=="icrg_qog")) %.%
   # filter(!grepl("yeara", coef.name) & coef.name != "(Intercept)") %.%
+  mutate(std.err=std.errs) %.%
+  mutate(upper=estimate + (std.err * 1.96)) %.%
+  mutate(lower=estimate - (std.err * 1.96)) %.%
   mutate(coef.name=factor(coef.name, 
-                          labels=c("Quality of governance", "Unified democracy score (UDS)")))
+                          labels=c("Quality of governance", "Unified democracy score (UDS)"))) %.%
+  mutate(odds.ratio=1-exp(estimate)) %.%
+  arrange(estimate)
 
 # Plot simulated coefficients of interest
-p <- ggplot(coefs.plot, aes(y=estimate, x=coef.name, fill=coef.name)) + 
-  geom_violin(scale="width") + geom_hline(yintercept=0) + coord_flip() + 
-  labs(x=NULL, y="Coefficient estimate (log odds)") + theme_bw(8) + 
+uds.sim <- ggplot(coefs.plot, aes(y=estimate, x=coef.name, fill=coef.name)) + 
+  geom_linerange(aes(ymin=lower, ymax=upper), size=4, alpha=0.008, color="black") + 
+  geom_violin(scale="width", colour=NA) + geom_hline(yintercept=0) + coord_flip() + 
+  labs(x=NULL, y="Coefficient estimate (log odds)") + theme_ath(8) + 
   guides(fill=FALSE)
-ggsave(p, filename="Figures/simulated_uds.pdf", width=6, height=3.5)
+uds.sim
+ggsave(uds.sim, filename="Figures/simulated_uds.pdf", width=6, height=3.5)
