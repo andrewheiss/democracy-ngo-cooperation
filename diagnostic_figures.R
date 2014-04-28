2e59f941b61d44083862b12e44cc145f9c7badae
Andrew Heiss <andrewheiss@gmail.com>
2014-04-27 21:37:26 -0400

    Clean out old unused plots

--- diagnostic_figures.R
+++ diagnostic_figures.R
@@ -3,36 +3,10 @@ source("load_data.R")
 library(ggplot2)
 library(reshape2)
 
-theme_ath <- function(base_size=12) {
-  ret <- theme_bw(base_size) + 
-    theme(axis.title=element_text(vjust=0.2), legend.position="bottom")
-  ret
-}
 
 #---------------------------
 # Initial diagnostic plots
 #---------------------------
-
-# Regime type and governance
-p <- ggplot(na.omit(coop.data[,c("icrg_qog", "chga_demo")]), 
-            aes(y=icrg_qog, x=chga_demo))
-p + geom_violin()
-
-# UDS scale and governance
-p <- ggplot(na.omit(coop.data[,c("uds_mean", "icrg_qog", "chga_demo")]), 
-            aes(x=uds_mean, y=icrg_qog, colour=chga_demo))
-p + geom_point()
-
-# Democracy duration
-p <- ggplot(na.omit(coop.data[,c("demdur", "icrg_qog", "ht_region", "Iyeara")]), 
-            aes(x=demdur, y=icrg_qog))
-p + geom_point(aes(colour=ht_region)) + facet_wrap(~ Iyeara)
-
-
-p <- ggplot(coop.data, aes(x=lngdppercap, y=icrg_qog, color=poor.dem))
-p + geom_point()
-
-
 plot.demdur <- ggplot(filter(coop.data, demdur > 0), aes(x=demdur, fill=ht_region)) + 
   geom_histogram(binwidth=1) + 
   coord_cartesian(xlim=c(0, 70)) + 
@@ -41,6 +15,7 @@ plot.demdur <- ggplot(filter(coop.data, demdur > 0), aes(x=demdur, fill=ht_regio
   theme_ath(8)
 plot.demdur
 ggsave(plot.demdur, filename="Figures/demdur.pdf", width=5, height=4, units="in")
+
 
 plot.uds <- ggplot(coop.data, aes(x=uds_mean, fill=ht_region)) + 
   geom_histogram(binwidth=0.05) + 
